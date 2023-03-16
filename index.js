0e32b1b2fd840de62710df5d1b882f4b7e391aa8
rcraig12 <rcraig12@gmail.com>
2023-03-16 12:55:53 +0000

    improved blachchain search

--- index.js
+++ index.js
@@ -5,23 +5,38 @@ const uniswapRouterABI = require('./ABI/UniswapRouterABI.json'); // load the ABI
 const uniswapFactoryABI = require('./ABI/uniswapFactoryABI.json');
 const uniswapV2PairABI = require('./ABI/uniswapV2PairABI.json');
 
+let latestBlock;
+
 let tokenData = {
   ethUSD: "",
   contract: "",
   symbol: "",
   name: "",
   supply: "",
-  pair: "",
-  holders: []
+  pair: {
+    address: "",
+    token0: {
+      name: "",
+      address: "",
+      supply: ""
+    },
+    token1: {
+      name: "",
+      address: "",
+      supply: ""
+    }
+  },
+  priceUSD: "",
+  totalLiquidity: "",
+  holders: [],
+  holderTotal: ""
 };
-
-
 
 // Initialize web3 provider
 const web3 = new Web3('https://mainnet.infura.io/v3/b6bf7d3508c941499b10025c0776eaf8'); // Replace with your Infura project ID
 
 // Set the contract address and token symbol
-const tokenAddress = '0x1E8Cc81Cdf99C060c3CA646394402b5249B3D3a0'; // Replace with the contract address of your token
+//const tokenAddress = '0x1E8Cc81Cdf99C060c3CA646394402b5249B3D3a0'; // Replace with the contract address of your token
 const tokenSymbol = 'VB'; // Replace with the symbol of your token
 
 const wethAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'; // WETH addres
@@ -43,15 +58,27 @@ const uniswapRouterContract = new web3.eth.Contract(uniswapRouterABI, uniswapRou
 const uniswapFactoryContract = new web3.eth.Contract(uniswapFactoryABI, uniswapFactoryAddress);
 
 // Create a new instance of the token contract
-const tokenContract = new web3.eth.Contract(tokenABI, tokenAddress);
+//const tokenContract = new web3.eth.Contract(tokenABI, tokenAddress);
 
 // Create a new instance of the Chainlink Price Feed Contract
 const priceFeedContract = new web3.eth.Contract(priceFeedABI, priceFeedContractAddress);
 
+//Get latest block on the chain
+const getLatestBlock = async () => {
+  web3.eth.getBlockNumber()
+  .then((blockNumber) => {
+    //console.log(`Latest block number on mainnet: ${blockNumber}`);
+    latestBlock = blockNumber;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+}
+
 // Get the Token Symbol
 const getTokenSymbol = async ( ca ) => {
 
-  const tokenContract = new web3.eth.Contract(tokenABI, tokenAddress);
+  const tokenContract = new web3.eth.Contract(tokenABI, ca);
   return await tokenContract.methods.symbol().call();
 
 }
@@ -59,7 +86,7 @@ const getTokenSymbol = async ( ca ) => {
 // Get the Toekn Name
 const getTokenName = async ( ca ) => {
 
-  const tokenContract = new web3.eth.Contract(tokenABI, tokenAddress);
+  const tokenContract = new web3.eth.Contract(tokenABI, ca);
   return await tokenContract.methods.name().call();
 
 }
@@ -67,7 +94,7 @@ const getTokenName = async ( ca ) => {
 // Get the Token Supply
 const getTokenSupply = async ( ca ) => {
 
-  const tokenContract = new web3.eth.Contract(tokenABI, tokenAddress);
+  const tokenContract = new web3.eth.Contract(tokenABI, ca);
   return await tokenContract.methods.totalSupply().call();
 
 }
@@ -82,6 +109,9 @@ const getTokenPair = async ( ca ) => {
 // Get the Token Liquidity pair details
 const getTokenPairDetail = async ( ca ) => {
 
+  const wethAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'; // WETH addres
+  const wethSymbol = 'WETH';
+
   let price,weth,token;
 
   const pairAddress = await uniswapFactoryContract.methods.getPair(wethAddress, ca).call();
@@ -89,7 +119,7 @@ const getTokenPairDetail = async ( ca ) => {
   const token0 = await pairContract.methods.token0().call();
   const token1 = await pairContract.methods.token1().call();
   const reserves = await pairContract.methods.getReserves().call();
-
+  
   if ( token0 === wethAddress ){
 
     tokenData.pair.token0.name = wethSymbol;
@@ -100,7 +130,7 @@ const getTokenPairDetail = async ( ca ) => {
     tokenData.pair.token1.supply = reserves[1];
     weth = reserves[0]; // WETH reserve
     token = reserves[1]; // token reserve
-    price = weth / token;
+    //price = weth / token;
 
   } else {
 
@@ -112,54 +142,51 @@ const getTokenPairDetail = async ( ca ) => {
     tokenData.pair.token1.supply = reserves[1];
     weth = reserves[1]; // WETH reserve
     token = reserves[0]; // token reserve
-    price = weth / token;
+    //price = weth / token;
 
   }
 
 }
 
-const getTokenPrice = async () => {
+const getTokenTotalLiquidity = async ( ca ) => {
+  
+  let tokenPriceUSD;
+
+  const pairAddress = await uniswapFactoryContract.methods.getPair(wethAddress, ca).call();
+  const pairContract = new web3.eth.Contract(uniswapV2PairABI, pairAddress);
+  const reserves = await pairContract.methods.getReserves().call();
+  const totalLiquidity = ( reserves[0] * tokenData.priceUSD) / (reserves[1] * tokenData.priceUSD );
+  return totalLiquidity
+
+}
+
+const getTokenPrice = async ( ca ) => {
 
   const wethAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'; // WETH addres
   const wethSymbol = 'WETH';
-  const pairAddress = await uniswapFactoryContract.methods.getPair(wethAddress, tokenAddress).call();
+  const pairAddress = await uniswapFactoryContract.methods.getPair(wethAddress, ca).call();
 
   const pairContract = new web3.eth.Contract(uniswapV2PairABI, pairAddress);
+  //const liquidityBalance = await pairContract.methods.balanceOf(pairAddress).call();
   const token0 = await pairContract.methods.token0().call();
   const token1 = await pairContract.methods.token1().call();
   const reserves = await pairContract.methods.getReserves().call();
   
+  //console.log(`!!! LP Balance : ${liquidityBalance}`);
+
   let price,weth,token;
 
   if (token0 === wethAddress ){
 
-    tokenData.pair.token0.name = wethSymbol;
-    tokenData.pair.token0.address = wethAddress;
-    tokenData.pair.token0.supply = reserves[0];
-    tokenData.pair.token1.name = tokenData.symbol;
-    tokenData.pair.token1.address = tokenData.contract;
-    tokenData.pair.token1.supply = reserves[1];
     weth = reserves[0]; // WETH reserve
     token = reserves[1]; // token reserve
     price = weth / token;
-    console.log(`${wethSymbol} : ${weth}`);
-    console.log(`${tokenSymbol} : ${token}`);
-    console.log(`${wethSymbol}/${tokenSymbol} ${price}`);
 
   } else {
 
-    tokenData.pair.token0.name = tokenData.symbol;
-    tokenData.pair.token0.address = tokenData.contract;
-    tokenData.pair.token0.supply = reserves[0];
-    tokenData.pair.token1.name = wethSymbol;
-    tokenData.pair.token1.address = wethAddress;
-    tokenData.pair.token1.supply = reserves[1];
     weth = reserves[1]; // WETH reserve
     token = reserves[0]; // token reserve
     price = weth / token;
-    console.log(`${wethSymbol} : ${weth}`);
-    console.log(`${tokenSymbol} : ${token}`);
-    console.log(`${tokenSymbol}/${wethSymbol} ${price}`);
 
   }
 
@@ -177,60 +204,95 @@ const getEthPrice = async () => {
 }
 
 
-const getHolders = async () => {
+const getHolders = async (ca) => {
 
-  let holderInfo = [];
+  console.log(`Please wait looking for blocks with transactions for contract ${ca}`);
+
+  const tokenContract = new web3.eth.Contract(tokenABI, ca);
+
+  let holderInfo = new Set();
 
   // Create an empty set to store the token holders
   const tokenHolders = new Set();
 
-  // Fetch all Transfer events for the token contract
-  tokenContract.getPastEvents('Transfer', { fromBlock: 0, toBlock: 'latest' }, (err, events) => {
-    
-    if (err) {
-    
-      console.error(err);
-      return;
-    
+  let startBlock = 0;
+
+  for( var block = 0; block < latestBlock; block += 10000){
+
+    if ( block > 10000){
+
+      startBlock = block - 10000;
+
     }
 
-    // Loop through each Transfer event and add the sender and recipient to the token holders set
-    for (let i = 0; i < events.length; i++) {
-      const sender = events[i].returnValues.from;
-      const recipient = events[i].returnValues.to;
+    if ( (latestBlock - block) < 10000 ) {
 
-      if (sender !== '0x0000000000000000000000000000000000000000') {
-        tokenHolders.add(sender.toLowerCase());
-      }
+      block = latestBlock;
 
-      if (recipient !== '0x0000000000000000000000000000000000000000') {
-        tokenHolders.add(recipient.toLowerCase());
-      }
     }
 
-    // Loop through the token holders set and print their address and balance
+    if ( block > latestBlock ) {
 
-    let holderCount = 0;
+      block = latestBlock;
 
-    tokenHolders.forEach(async (address) => {
+    }
+
+    console.log(`Block ${block} latest ${latestBlock}`);
+
+    await tokenContract.getPastEvents('Transfer', { fromBlock: startBlock, toBlock: block }, (err, events) => {
       
-      const balance = await tokenContract.methods.balanceOf(address).call();
+      if (err) {
+      
+        console.error(err);
+        return;
+      
+      }
 
-      if (balance > 0) {
+      // Loop through each Transfer event and add the sender and recipient to the token holders set
+      for (let i = 0; i < events.length; i++) {
+        const sender = events[i].returnValues.from;
+        const recipient = events[i].returnValues.to;
 
-        holderCount++;
-        // Print the token holder's address and balance
-        //console.log(`Holder: ${holderCount} Address: ${address}, Balance: ${balance} ${tokenSymbol}`);
-        holderInfo.push({address: address, balance: balance});
-        //console.log(holders)
+        if (sender !== '0x0000000000000000000000000000000000000000') {
+          tokenHolders.add(sender.toLowerCase());
+        }
+
+        if (recipient !== '0x0000000000000000000000000000000000000000') {
+          tokenHolders.add(recipient.toLowerCase());
+        }
 
       }
 
     });
 
+    
+
+  }
+
+  console.log(`Address Count : ${tokenHolders.size} between block 0 and ${block}`);
+
+  //let holderCount;
+
+  // Loop through the token holders set and print their address and balance
+  tokenHolders.forEach( async (address) => {
+
+    const balance = await tokenContract.methods.balanceOf(address).call();
+
+    if (balance > 0) {
+
+      // Print the token holder's address and balance
+      //console.log(`Holder: ${holderCount} Address: ${address}, Balance: ${balance} ${tokenSymbol}`);
+      await holderInfo.add({address: address, balance: balance});
+      //holderCount++;
+      //console.log(holders)
+
+    }
+
   });
 
-  console.log(holderInfo);
+  // Fetch all Transfer events for the token contract
+
+  //tokenData.holderTotal = tokenData.holders.length;
 
   return holderInfo;
 
@@ -239,9 +301,11 @@ const getHolders = async () => {
 
 const getTokenUsingContract = async ( ca ) => {
 
+  await getLatestBlock();
+
   tokenData.contract = ca;
 
-  await getTokenSymbol(ca).then( res => {
+  await getTokenSymbol( ca ).then( res => {
 
     tokenData.symbol = res;
 
@@ -251,7 +315,7 @@ const getTokenUsingContract = async ( ca ) => {
 
   });
 
-  await getTokenName(ca).then( res => {
+  await getTokenName( ca ).then( res => {
 
     tokenData.name = res;
 
@@ -261,7 +325,7 @@ const getTokenUsingContract = async ( ca ) => {
 
   });
 
-  await getTokenSupply(ca).then( res => {
+  await getTokenSupply( ca ).then( res => {
 
     tokenData.supply = res;
 
@@ -271,7 +335,7 @@ const getTokenUsingContract = async ( ca ) => {
 
   });
 
-  await getTokenSymbol(ca).then( res => {
+  await getTokenSymbol( ca ).then( res => {
 
     tokenData.symbol = res;
 
@@ -281,21 +345,9 @@ const getTokenUsingContract = async ( ca ) => {
 
   });
 
-  await getTokenPair(ca).then( res => {
+  await getTokenPair( ca ).then( res => {
 
-    tokenData.pair = { 
-      address: res,
-      token0: {
-        name: '',
-        address: "",
-        supply: ""
-      },
-      token1: {
-        name: "",
-        address: "",
-        supply: ""
-      }
-    };
+    tokenData.pair.address = res;
 
   }).catch(err => {
 
@@ -303,10 +355,33 @@ const getTokenUsingContract = async ( ca ) => {
 
   });
 
-  await getHolders().then( res => {
+  await getTokenPairDetail( ca );
 
-    console.log(res);
+  await getTokenPrice( ca ).then( priceUSD => {
+
+    tokenData.priceUSD = priceUSD;
+
+  }).catch ( err => {
+
+    console.error(err);
+
+  });
+
+  await getTokenTotalLiquidity( ca ).then( res => {
+
+    tokenData.totalLiquidity = res / 10000000000;
+
+  }).catch(err => {
+
+    console.log(err);
+
+  });
+
+  await getHolders( ca ).then( res => {
+
+    //console.log(res);
     tokenData.holders = res;
+    tokenData.holderTotal = res.size;
 
   }).catch(err => {
 
@@ -318,7 +393,7 @@ const getTokenUsingContract = async ( ca ) => {
   await getEthPrice().then((ethPrice) => {
 
     tokenData.ethUSD = ethPrice;
-    console.log(`ETH price in USD: $${ethPrice}`);
+    //console.log(`ETH price in USD: $${ethPrice}`);
 
   }).catch((err) => {
 
@@ -326,23 +401,35 @@ const getTokenUsingContract = async ( ca ) => {
 
   });
 
-  await getTokenPrice().then((ethPrice) => {
+  // await getTokenPrice().then((ethPrice) => {
 
-    console.log(`Token price in USD: $${ethPrice}`);
+  //   console.log(`Token price in USD: $${ethPrice}`);
 
-  }).catch((err) => {
+  // }).catch((err) => {
 
-    console.error(err);
+  //   console.error(err);
 
-  });
+  // });
 
   return tokenData;
 
 }
 
-getTokenUsingContract( '0x1E8Cc81Cdf99C060c3CA646394402b5249B3D3a0' ).then( res => {
+// getTokenUsingContract( '0x1E8Cc81Cdf99C060c3CA646394402b5249B3D3a0' ).then( res => {
+
+//   //console.log(res);
+//   console.log(`token data sampled successfully`);
+
+// }).catch(err => {
+
+//   console.log(err);
+
+// });
+
+getTokenUsingContract( '0x24b20da7a2fa0d1d5afcd693e1c8afff20507efd' ).then( res => {
 
   console.log(res);
+  //console.log(`token data sampled successfully`);
 
 }).catch(err => {
 
